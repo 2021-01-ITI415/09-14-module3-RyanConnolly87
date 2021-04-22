a0b5adb285394148fc10329f52bdf8b07e4bea68
RyanConnolly87 <RyanConnolly87@users.noreply.github.com>
2021-04-22 01:19:34 -0400

    made it where the enemy music plays when enemy is in certain range of the hero

--- Assets/_Labs/05-Audio/PlayerAudio.cs
+++ Assets/_Labs/05-Audio/PlayerAudio.cs
@@ -13,6 +13,31 @@ public class PlayerAudio : MonoBehaviour
     public AudioMixerSnapshot idleSnapshot;
     public AudioMixerSnapshot auxInSnapshot;
 
+    public LayerMask enemyMask;
+
+    bool enemyNear;
+    
+    private void Update()
+    {
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 5f, transform.forward, 0f, enemyMask);
+        if(hits.Length > 0)
+        {
+            if (!enemyNear)
+            {
+                auxInSnapshot.TransitionTo(0.5f);
+                enemyNear = true;
+            }
+        }
+        else
+        {
+            if (enemyNear)
+            {
+                idleSnapshot.TransitionTo(0.5f);
+                enemyNear = false;
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Water"))
