8c76601f0bb68792a298f13a073511ab7fb378fb
RyanConnolly87 <RyanConnolly87@users.noreply.github.com>
2021-04-22 02:14:26 -0400

    Made it where having a enemy near you did not mess up the victory audio

--- Assets/_Labs/05-Audio/AudioManager.cs
+++ Assets/_Labs/05-Audio/AudioManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager manager;
@@ -14,7 +14,32 @@ public class AudioManager : MonoBehaviour
     public AudioSource mainMusic;
     public AudioSource auxMusic;
     public AudioSource ambSound;
-    
-    
+
+    public AudioSource eventMusic;
+
+    public AudioMixerSnapshot eventSnap;
+    public AudioMixerSnapshot idleSnapshot;
+
+    public bool eventRunning;
+    public bool auxIn;
+
+    public AudioMixerSnapshot currentAudioMixerSnapshot;
+
+    public IEnumerator PlayEventMusic()
+    {
+        eventRunning = true;
+        eventSnap.TransitionTo(0.25f);
+        currentAudioMixerSnapshot = eventSnap;
+        yield return new WaitForSeconds(0.3f);
+        eventMusic.Play();
+        while (eventMusic.isPlaying)
+        {
+
+            yield return null;
+        }
+        eventRunning = false;
+        idleSnapshot.TransitionTo(0.5f);
+        yield break;
+    }
    
 }
